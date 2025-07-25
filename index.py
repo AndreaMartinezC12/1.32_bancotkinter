17a53aa830fd2959e797f59b8b397cc462dffeb3
AndreaMartinezC <andrea12_94@hotmail.com>
2025-07-24 19:00:27 -0600

    Agregando base para el ejercicio

---
+++ index.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+from tkinter import ttk,messagebox
+
+root=tk.Tk()
+root.title("Banco")
+root.geometry("300x200")
+tk.Label(root, text="Ejercicio Banco").pack()
+
+root.mainloop()
